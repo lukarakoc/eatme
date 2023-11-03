e99d583ba5bc37120ace779598f2622ff6b25815
Luka Rakocevic <rakocevic.luka@gmail.com>
2023-11-03 02:15:25 +0100

    v1.02 - Password admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -16,16 +15,18 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::insert([
-            'name' => 'Luka Rakocevic',
-            'email' => 'rakocevic.luka@gmail.com',
-            'password' => Hash::make('Rakoc211009'),
-            'role_id' => Role::ADMIN
-        ],
-        [
-            'name' => 'Eat.me - Matija',
-            'email' => 'eatme@dmline.me',
-            'password' => Hash::make("eatmesoftware"),
-            'role_id' => Role::ADMIN
+            [
+                'name' => 'Luka Rakocevic',
+                'email' => 'rakocevic.luka@gmail.com',
+                'password' => Hash::make('Rakoc211009'),
+                'role_id' => Role::ADMIN
+            ],
+            [
+                'name' => 'Eat.me - Matija',
+                'email' => 'eatme@dmline.me',
+                'password' => Hash::make("eatmesoftware"),
+                'role_id' => Role::ADMIN
+            ]
         ]);
     }
 }
